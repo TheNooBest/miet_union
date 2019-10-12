2faf17c9bb612dba01843f048fdeff047f117241
Hearthstowner <lazarev989@gmail.com>
2019-10-13 00:58:32 +0300

    feat: breadcrumbs

--- getdiary/settings.py
+++ getdiary/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_bootstrap_breadcrumbs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'view_breadcrumbs',
     # our apps
     'getdiary'
 ]
