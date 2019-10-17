0c08a46c82b055344bc248491506624cd0887cc2
Hearthstowner <lazarev989@gmail.com>
2019-10-17 14:01:04 +0300

    feat: ourteampage

--- miet_union/urls.py
+++ miet_union/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import home, test
+from .views import (
+    home,
+    ourteam,
+    test,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('ourteam', ourteam, name='ourteam'),
     path('test', test, name='test'),
 ]
